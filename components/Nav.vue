f083103858ac1199786cc30c69cd557d9ad98778
Stanton Huang <stantonhuang11@gmail.com>
2020-07-19 15:53:30 -0700

    changes navbar to use nuxt-link

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">NuxtApp</a>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nuxt-link class="navbar-brand" to="/">NuxtApp</nuxt-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home</a>
+          <nuxt-link class="nav-link" to="/">Home</nuxt-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="users">Users</a>
+          <nuxt-link class="nav-link" to="/users">Users</nuxt-link>
         </li>
       </ul>
     </div>
