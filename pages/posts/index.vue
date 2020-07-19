b12f2e045e940253a185fa3970ef196af478cf1e
Stanton Huang <stantonhuang11@gmail.com>
2020-07-19 16:23:03 -0700

    adds async the vue way using axios and vue hooks

---
+++ pages/posts/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>making api requests without nuxt</h2>
+
+
+    <div class="container">
+      <h4 v-for="post in posts" v-bind:key="post.id">
+        {{post.title}}
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then(response => {
+        console.log(response);
+        this.posts = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+}
+</script>
