8c1326c7f4fa0fe2632af23180c7620927b40b97
Stanton Huang <stantonhuang11@gmail.com>
2020-07-19 15:09:36 -0700

    initial setup with a nested frontend nuxt route+validations

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>this is user {{$route.params.id}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  validate({params}) { //destructured context to use params. usually would use context :)
+    return /^\d+$/.test(params.id)  //regex forcing one or more digits 
+  }
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>users index</h1>
+
+    <input type="text" v-model="id">
+    <button @click="loadUser">Load User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: ''
+    }
+  },
+  methods: {
+    loadUser() {
+      this.$router.push(`/users/${this.id}`);
+    }
+  }
+}
+</script>
