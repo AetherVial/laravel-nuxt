0e63487df5dec2658d3edd19987d067347ecb249
Stanton Huang <stantonhuang11@gmail.com>
2020-07-20 12:07:29 -0700

    adds async function to card component

--- components/Card.vue
+++ components/Card.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
     <div class="card bg-light mb-3" style="max-width: 18 rem;">
       <div class="card-header">{{post.id}}</div>
-      <h5 class="card-title">{{post.title}}</h5>
+      <h5 class="card-title">
+        <nuxt-link :to='{name: "posts-id", params: {id: post.id}}'>{{post.title}}</nuxt-link>
+      </h5>
       <p class="card-text">
         lorem ipsum bullshit
       </p>
     </div>
-  </div>
 </template>
 
 <script>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>{{post.title}}</h1>
+    <pre>{{post.body}}</pre>
+    <nuxt-link to="/posts">Back</nuxt-link>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      post: ''
+    }
+  },
+  async asyncData(context) {
+    let {data} = await axios.get(`https://jsonplaceholder.typicode.com/todos/${context.params.id}`)
+    return {post: data}
+  }
+}
+</script>
