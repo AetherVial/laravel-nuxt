ee8ce5ce570ca2c97ebdb878ad5f950569395b0a
Stanton Huang <stantonhuang11@gmail.com>
2020-07-20 14:46:12 -0700

    prep for firebase deploy

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -29,11 +29,11 @@ import {mapGetters} from 'vuex'
       // }
       ...mapGetters(['posts']) //can access multiple getters using mapgetters
     },
-    // async fetch({store}) {
-    //   let {data} = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    //   //return allPosts()
-    //   store.dispatch('setPosts', data)
-    // },
+    async fetch({store}) {
+      let {data} = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      //return allPosts()
+      store.dispatch('setPosts', data)
+    },
     head: {
       title: 'List of Posts'
     }
